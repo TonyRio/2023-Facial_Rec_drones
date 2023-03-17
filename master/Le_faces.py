16d08ad611e64d2ec6842536dbdcad7dc023fa9a
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2023-03-16 21:41:16 -0300

    testes

---
+++ master/Le_faces.py
@@ -0,0 +1,47 @@
+import cv2
+import datetime
+
+# carrega o classificador haar cascade pré-treinado para detecção de faces
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# abre a webcam
+cap = cv2.VideoCapture(0)
+
+# loop infinito
+while True:
+    # lê o frame da webcam
+    ret, frame = cap.read()
+
+    # converte o frame para escala de cinza para melhor desempenho
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detecta as faces no frame usando o classificador haar cascade
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # loop sobre cada face detectada
+    for (x, y, w, h) in faces:
+        # desenha um retângulo verde em cada face detectada
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # reconhece o rosto e obtém o nome
+        # você precisa implementar essa parte com o reconhecimento facial
+        name = "Desconhecido"
+
+        # obtém a hora atual do sistema
+        now = datetime.datetime.now()
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        # desenha o nome e a hora atual na tela
+        cv2.putText(frame, name, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.putText(frame, timestamp, (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # exibe o frame com as faces detectadas
+    cv2.imshow('Video', frame)
+
+    # espera por uma tecla para sair
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# libera os recursos utilizados
+cap.release()
+cv2.destroyAllWindows()
--- master/testes.py
+++ master/testes.py
@@ -1,33 +1,61 @@
 import cv2
-from matplotlib import pyplot
+import numpy as np
 
-# carrega o classificador haar cascade pré-treinado para detecção de faces
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Carrega o modelo YOLO
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
 
-# abre a webcam
+# Define as classes que o modelo pode identificar
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+# Configura a webcam
 cap = cv2.VideoCapture(0)
 
 while True:
-    # lê o frame da webcam
+    # Captura um quadro da webcam
     ret, frame = cap.read()
 
-    # converte o frame para escala de cinza para melhor desempenho
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Cria um blob de imagem para o modelo YOLO
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255, (416, 416), (0, 0, 0), True, crop=False)
 
-    # detecta as faces no frame usando o classificador haar cascade
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    # Passa o blob de imagem para o modelo YOLO
+    net.setInput(blob)
 
-    # desenha um retângulo verde em cada face detectada
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Obtém as detecções
+    outs = net.forward(net.getUnconnectedOutLayersNames())
 
-    # exibe o frame com as faces detectadas
-    #cv2.imshow('Video', frame)
-    pyplot.imshow('Video', frame)
-    # espera por uma tecla para sair
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Processa as detecções
+    for detection in outs:
+        for obj in detection:
+            # Obtém as informações da classe e confiança da detecção
+            scores = obj[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            # Verifica se a detecção tem uma confiança suficientemente alta
+            if confidence > 0.5:
+                # Obtém as coordenadas da detecção
+                center_x = int(obj[0] * frame.shape[1])
+                center_y = int(obj[1] * frame.shape[0])
+                width = int(obj[2] * frame.shape[1])
+                height = int(obj[3] * frame.shape[0])
+
+                # Calcula as coordenadas do canto superior esquerdo
+                x = int(center_x - width / 2)
+                y = int(center_y - height / 2)
+
+                # Desenha o retângulo da detecção e o texto da classe
+                cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
+                cv2.putText(frame, classes[class_id], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Mostra a imagem com as detecções
+    cv2.imshow("Webcam", frame)
+
+    # Aguarda a tecla 'q' para sair
+    if cv2.waitKey(1) == ord('q'):
         break
 
-# libera os recursos utilizados
+# Libera a webcam e fecha a janela
 cap.release()
 cv2.destroyAllWindows()
