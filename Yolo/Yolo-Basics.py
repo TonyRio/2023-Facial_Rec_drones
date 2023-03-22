bce4321c9522fe2a7b344b4532baf6111c3c8605
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2023-03-22 19:35:23 -0300

    detect

--- Yolo/Yolo-Basics.py
+++ Yolo/Yolo-Basics.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('yolov8l.pt')
-results = model("Images/1.png", show=True)
-#cv2.waitKey(0)
+model = YOLO('../Yolo-Weights/yolov8l.pt')
+results = model("Images/3.png", show=True)
+cv2.waitKey(0)
