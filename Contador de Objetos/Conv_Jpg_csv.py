df53f0b0973d3662f7bf9e47d80db6395aec9a1e
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2023-03-28 14:50:14 -0300

    cont_car

--- Contador de Objetos/Conv_Jpg_csv.py
+++ Contador de Objetos/Conv_Jpg_csv.py
@@ -1,41 +1,39 @@
 import cv2
 import pandas as pd
 import os
-import csv
+
 # Definir caminho da pasta com as imagens
 folder_path = '\Carros\cars_train'
 
 # Criar lista para armazenar DataFrames
 dfs = []
 
-
 # Iterar sobre as imagens na pasta
 for filename in os.listdir(folder_path):
     # Verificar se o arquivo é uma imagem
-
     if filename.endswith('.jpg'):
         # Carregar imagem
-
         img = cv2.imread(os.path.join(folder_path, filename))
-
         # Converter para escala de cinza
-
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
         # Achatar em matriz unidimensional
         flat = gray.flatten()
-
         # Criar DataFrame com a matriz unidimensional
         df = pd.DataFrame(flat)
         # Adicionar DataFrame à lista
-
         dfs.append(df)
-
-        print(len(dfs))
+        print(dfs)
 
 # Concatenar DataFrames em um único DataFrame
 result_df = pd.concat(dfs, axis=1)
 
 # Salvar DataFrame em arquivo CSV
-result_df.to_csv('img_cars.csv', index=False, header=False)
+file_path = 'arquivo.csv'
+
+# verifica se o arquivo já existe
+if not os.path.isfile(file_path):
+    # se o arquivo não existir, cria o arquivo
+    result_df.to_csv(file_path, index=False)
+    print(f'Arquivo "{file_path}" criado com sucesso!')
+else:
+    print(f'O arquivo "{file_path}" já existe!')
