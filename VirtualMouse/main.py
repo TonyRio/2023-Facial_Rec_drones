2fa049e28a0aea9aaf2cb842615617d31b6fffb6
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2023-04-05 23:44:41 -0300

    cont

---
+++ VirtualMouse/main.py
@@ -0,0 +1,21 @@
+import cv2
+import mediapipe as mp
+
+
+cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+while True:
+    _, frame = cap.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    if hands:
+
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame, hand)
+
+    print(hands)
+    cv2.imshow("Virtual Mouse", frame)
+
+    cv2.waitKey(1)
